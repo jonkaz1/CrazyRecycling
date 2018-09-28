a65be22af4587bf2272a88419b221111e76775cd
jonkaz1 <j.kazlauskas@ktu.edu>
2018-09-28 15:18:18 +0300

    Simple movement

--- CrazyRecycling/Form1.Designer.cs
+++ CrazyRecycling/Form1.Designer.cs
@@ -28,13 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(103, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- CrazyRecycling/Form1.cs
+++ CrazyRecycling/Form1.cs
@@ -16,5 +16,38 @@ namespace CrazyRecycling
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int x = textBox1.Location.X;
+            int y = textBox1.Location.Y;
+
+            if (e.KeyCode == Keys.W)
+            {
+                y--;
+                textBox1.Text = "W";
+            }
+            else if (e.KeyCode == Keys.A)
+            {
+                x--;
+                textBox1.Text = "A";
+            }
+            else if (e.KeyCode == Keys.S)
+            {
+                y++;
+                textBox1.Text = "S";
+            }
+            else if (e.KeyCode == Keys.D)
+            {
+                x++;
+                textBox1.Text = "D";
+            }
+            textBox1.Location = new Point(x, y);
+        }
     }
 }

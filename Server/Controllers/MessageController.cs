42f9c48d68983d4e3fb8c60789edcf59c511667e
Paulius Puleikis <33981391+paupul50@users.noreply.github.com>
2018-12-13 19:44:36 +0200

    Interpreter done

--- Server/Controllers/MessageController.cs
+++ Server/Controllers/MessageController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Server.Helper.MessageInterpreter;
 using Server.Models;
 
 namespace Server.Controllers
@@ -24,17 +25,22 @@ namespace Server.Controllers
         [HttpGet]
         public IActionResult GetComment()
         {
-            var messages = _context.Message; //.Include(m => m.Player);
+            var messages = _context.Message.Include(m => m.Player);
             return Ok(messages);
         }
 
         [HttpPost]
         public IActionResult CreateComment([FromBody]MessageDto messageDto)
         {
+            MessageInterpreter interpreter = new MessageInterpreter();
             var message = new Message();
+            Player player = _context.Player.Find(messageDto.PlayerId);
+            var messageContext = new MessageContext(player, messageDto.Message);
+            interpreter.ProcessMessage(ref messageContext);
             message.Created_At = DateTime.Now;
-            message.PlayerMessage = messageDto.Message;
-//           message.Player = _context.Player.Find(messageDto.PlayerId);
+            message.PlayerMessage = messageContext.Message;
+            message.Player = messageContext.Player;
+            _context.Player.Find(messageContext.Player.PlayerId).HealthPoints = messageContext.Player.HealthPoints;
             _context.Message.Add(message);
             _context.SaveChanges();
             return Created("Created", message);
